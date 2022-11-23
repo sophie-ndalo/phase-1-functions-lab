37e74e73d357920c9bf97e427b6a9bd98888d188
sophia <sophiendalo@gmail.com>
2022-11-23 22:39:59 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    if (distance>42){
+        return distance-42
+    } else {return 42-distance}
+}
+
+function distanceFromHqInFeet(feet){
+   return distanceFromHqInBlocks(feet)*264
+}
+
+function distanceTravelledInFeet(start,destination){
+    return Math.abs((destination-start)*264)
+}
+
+function calculatesFarePrice(start,destination){
+    if (distanceTravelledInFeet(start,destination)<=400){return 0}
+    else if(distanceTravelledInFeet(start,destination)<2000){return (distanceTravelledInFeet(start,destination)-400)*0.02}
+    else if(distanceTravelledInFeet(start,destination)>2500){return 'cannot travel that far'}
+    else{return 25}
+}
